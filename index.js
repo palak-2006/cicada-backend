6efc748f97f8a0bed495ee12140578bfad024a10
lightning-sagar <lightningsagar0@gmail.com>
2025-11-05 17:08:27 +0530

    added the cors

--- index.js
+++ index.js
@@ -8,7 +8,17 @@ import cors from "cors"
 
 const app = express();
 
-app.use(cors())
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:3000",
+      "https://cicada-frontend.onrender.com"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,  
+  })
+);
 dotenv.config();
 
 connectDB();
