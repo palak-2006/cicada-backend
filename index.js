a8e359cc79e0c042708de8f505670c88f966233f
lightning-sagar <lightningsagar0@gmail.com>
2025-11-05 17:09:23 +0530

    added the cors and added the versel link

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:3000",
-      "https://cicada-frontend.onrender.com"
+      "https://cicada-frontend.onrender.com",
+      "https://cicada-old.vercel.app"
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,  
