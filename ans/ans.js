2c7d518402c7865f5218295e2199fdb0190a1049
lightning-sagar <lightningsagar0@gmail.com>
2025-11-06 16:49:50 +0530

    added the ans

---
+++ ans/ans.js
@@ -0,0 +1,28 @@
+export const Ans = {
+  1: {
+    answer: "Tachyon 2025",
+  },
+  2: {
+    a: { answer: "ROSCORE" },
+    b: { answer: "BROADCASTER" },
+  },
+  3: {
+    a: { answer: "00300051261" },
+    b: { answer: "SORTONIP" },
+  },
+  4: {
+    answer: "ALLSPARK",
+  },
+  5: {
+    a: { answer: "00300051261" },
+    b: { answer: "SORTONIP" },
+  },
+  6: {
+    a: { answer: "00300051261" },
+    b: { answer: "SORTONIP" },
+  },
+  7: {
+    a: { answer: "00300051261" },
+    b: { answer: "SORTONIP" },
+  },
+};
